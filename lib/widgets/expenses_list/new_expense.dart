71a7ea9595d35af1709f2b28d438aa56569d943c
Jude <judejeevan.g1@gmail.com>
2023-12-03 16:32:07 +0530

    added the date selection feature and the dropdown button to select the type of the expense

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:expense_tracker/widgets/expense_home.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -8,18 +11,109 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context, firstDate: firstDate, lastDate: now);
+    setState(() {
+      selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          maxLength: 50,
-          decoration: InputDecoration(
-            label: Text('Title'),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            controller: _titleController,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
           ),
-        ),
-      ],),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  maxLength: 6,
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefix: Text('\$ '),
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const Gap(16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(selectedDate == null
+                        ? 'Select date'
+                        : formatter.format(selectedDate!)),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const Gap(16),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          child: Text(category.name.toUpperCase()),
+                          value: category,
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+                  const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('add expense'),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
